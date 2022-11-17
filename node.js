5f256b94f9a21b3c9af0524eaae5007deec5a215
Artik00 <laptenko_artem@mail.ru>
2022-11-17 21:36:44 +0300

    add documentation

--- node.js
+++ node.js
@@ -1,7 +1,12 @@
 const range = +process.argv[2];
 const arr = [];
 const colors = require("colors/safe");
-
+/**
+  * Данная функция проверяет простое ли число
+  * @param {number} n - Любое число
+  * @returns {boolean} Функция возвращает true если число простое и false если
+  * число составное
+  */
 function isPrime(n) {
     for (let i = 2, s = Math.sqrt(n); i <= s; i++)
         if (n % i === 0) return false;
@@ -20,7 +25,8 @@ if (range <= 0) {
             arr.push(i);
         }
     }
-
+    //  выводим на экран полученный массив, и подсвечиваем каждую цифру 
+    //  определенным цветом
     for (let i = 0; i < arr.length; i++) {
         let color = 'red';
         if (i === 0 || i % 3 === 0) {
